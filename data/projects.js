5c1b44f3fb2c4421747e34bbbf1fff74344b2215
oukaxbom <105546641+oukaxbom@users.noreply.github.com>
2026-04-19 22:43:29 +0900

    Create projects.js

---
+++ data/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+  {
+    id: "food",
+    title: "フード系",
+    image: "/images/img1.jpg",
+    works: [
+      {
+        id: "marshmallow",
+        title: "マシュマロ",
+        model: "/models/marshmallow.glb"
+      }
+    ]
+  }
+];
