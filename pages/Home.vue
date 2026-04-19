af1e850a3cb178641188b43984772713b709dbb4
oukaxbom <105546641+oukaxbom@users.noreply.github.com>
2026-04-19 23:08:07 +0900

    Update Home.vue

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,19 +1,3 @@
 <template>
-  <Header />
-
-  <!-- 3D -->
-  <ThreeViewer model="models/main.glb" />
-
-  <!-- 企画一覧 -->
-  <div class="grid">
-    <Card
-      v-for="project in projects"
-      :key="project.id"
-      :image="project.image"
-      :title="project.title"
-      @click="$router.push('/project/' + project.id)"
-    />
-  </div>
-
-  <Footer />
+  Home
 </template>
