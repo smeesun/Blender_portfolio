5e75ece3ab2904287ccb9fe915c0ddd89e1e7273
oukaxbom <105546641+oukaxbom@users.noreply.github.com>
2026-04-19 22:39:51 +0900

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,1 +1,11 @@
+<template>
+  <footer @click="scrollTop">
+    ↑ TOP
+  </footer>
+</template>
 
+<script setup>
+function scrollTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
+</script>
