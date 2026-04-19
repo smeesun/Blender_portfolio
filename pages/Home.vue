5f63a4b3286672cc8df6a490d90a73cae152d62a
oukaxbom <105546641+oukaxbom@users.noreply.github.com>
2026-04-19 22:31:35 +0900

    Add template structure to Home.vue

--- pages/Home.vue
+++ pages/Home.vue
@@ -1,1 +1,19 @@
+<template>
+  <Header />
 
+  <!-- 3D -->
+  <ThreeViewer model="models/main.glb" />
+
+  <!-- 企画一覧 -->
+  <div class="grid">
+    <Card
+      v-for="project in projects"
+      :key="project.id"
+      :image="project.image"
+      :title="project.title"
+      @click="$router.push('/project/' + project.id)"
+    />
+  </div>
+
+  <Footer />
+</template>
