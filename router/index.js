19914a12ac3d05fbb152ed9308cf953591d34b54
oukaxbom <105546641+oukaxbom@users.noreply.github.com>
2026-04-19 22:24:35 +0900

    Set up Vue Router with Home, Project, and Work routes

--- router/index.js
+++ router/index.js
@@ -1,1 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/Home.vue'
+import Project from '../pages/Project.vue'
+import Work from '../pages/Work.vue'
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/project/:id', component: Project },
+  { path: '/work/:id', component: Work }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes
+})
