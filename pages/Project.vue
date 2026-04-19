6db530a511a466eb7d7ab95e5ffdf47befb8314e
oukaxbom <105546641+oukaxbom@users.noreply.github.com>
2026-04-19 22:37:48 +0900

    Update Project.vue

--- pages/Project.vue
+++ pages/Project.vue
@@ -1,1 +1,18 @@
+<template>
+  <Header />
 
+  <h2>{{ project.title }}</h2>
+  <p>{{ project.desc }}</p>
+
+  <div class="grid">
+    <Card
+      v-for="work in project.works"
+      :key="work.id"
+      :image="work.image"
+      :title="work.title"
+      @click="$router.push('/work/' + work.id)"
+    />
+  </div>
+
+  <Footer />
+</template>
